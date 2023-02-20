238f25966a747e49a92051e806ab3caaab9e602a
austinjnr <austinobonyo@gmail.com>
2023-02-20 15:52:49 +0300

    prints out articles

--- main.rb
+++ main.rb
@@ -10,14 +10,28 @@ magazine1 = Magazine.new("NPr TinyDesk", "Music")
 magazine2 = Magazine.new("NYC Times", "Business and Treands")
 
 # Adds articles
-article1 = Article.new(author, magazine1, "Her Album preview")
+article1 = Article.new(author, magazine1, "Her Loss Album preview")
 article2 = Article.new(author1, magazine2, "police brutality")
 article3 = Article.new(author, magazine1, "Upcoming artists")
 article4 = Article.new(author1, magazine2, "The mass job Layoffs and resenssion")
 
 # methods tested
-puts Author.all
+ puts Author.all #Output:(author and author1)
  puts "The first publisher is #{author.name}"
-
  puts "The second publisher is #{author1.name}"
+
+ puts "There are two magazine under different categories"
+ puts "The first type is #{magazine1.name} and it covers the #{magazine1.category } sector"
+ puts "The second type is #{magazine2.name} and it covers the #{magazine2.category } sector"
+
+# Output: (article1, article2, article3, article4)
+puts "This are the articles in #{magazine1.name} and #{magazine2.name} magazines "
+ puts "1.#{article1.title}" 
+ puts "2.#{article2.title}"  
+ puts "3.#{article3.title}" 
+ puts "4.#{article4.title}" 
+
+ puts author.articles # Output: [article1, article3]
+ puts article1.author.name
+ puts author.magazines.title # Output:  magazine1
 
