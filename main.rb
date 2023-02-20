b1b0efd741d859799c8ed779d587398ba8b2323e
austinjnr <austinobonyo@gmail.com>
2023-02-20 12:29:05 +0300

    created magazines

--- main.rb
+++ main.rb
@@ -3,8 +3,11 @@ require_relative "./magazine_domain/author.rb"
 require_relative "./magazine_domain/magazine.rb"
 
 # Create author
-author = Author.new("Austin Obonyo")
+author = Author.new("Austin Junior")
+author1 = Author.new("Adams Kaws")
 # Created magazines
+magazine1 = Publisher.new("NPr TinyDesk", "XXL Freashmen")
+magazine2 = Publisher.new("NYC Times", "The People")
 
 # Adds articles
 
