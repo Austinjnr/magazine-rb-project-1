7d3eb4420d230344603e3b11b12394ee0572a81f
austinjnr <austinobonyo@gmail.com>
2023-02-20 12:47:21 +0300

    created articles

--- main.rb
+++ main.rb
@@ -10,7 +10,10 @@ magazine1 = Publisher.new("NPr TinyDesk", "XXL Freashmen")
 magazine2 = Publisher.new("NYC Times", "The People")
 
 # Adds articles
-
+article1 = Article.new(author, magazine1, "Her Album preview")
+article2 = Article.new(author1, magazine2, "police brutality")
+article3 = Article.new(author, magazine1, "Upcoming artists")
+article4 = Article.new(author1, magazine2, "The mass job Layoffs and resenssion")
 
 # methods tested
  
